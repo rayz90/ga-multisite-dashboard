1d48c95f82d21bb3e490696453306bb7aeccd08a
Raymond Vermaas <github@raymondvermaas.nl>
2014-11-02 21:55:26 +0100

    Added front-end

--- docroot/index.php
+++ docroot/index.php
@@ -1,4 +1,5 @@
 <?php
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -10,13 +11,7 @@ $app = new Silex\Application();
 
 /* START CONFIGURATION */
 $app['debug'] = true;
-
-$app['google_api_app_name'] = 'xxxx';
-$app['google_api_username'] = 'xxxx@developer.gserviceaccount.com';
-$app['google_api_key_path'] = __DIR__.'/../config/key.p12';
-$app['google_api_key_secret'] = 'notasecret';
-
-$app['websites_path'] = '../config/websites.json';
+$app['config_path'] = __DIR__.'/../config/config.json';
 /* END CONFIGURATION */
 
 /*
@@ -32,25 +27,42 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->register(new Silex\Provider\SessionServiceProvider());
 
+$app['config'] = $app->share(function ($app) {
+    $config = array();
+    try {
+        if (!file_exists($app['config_path'])) {
+            throw new FileNotFoundException('Could not find file'.$app['config_path']);
+        }
+        $json = file_get_contents($app['config_path']);
+        $config = json_decode($json);
+    } catch (Exception $e) {
+        $app['monolog']->addError($e->getMessage());
+        exit(1);
+    }
+
+    return $config;
+});
+
 /**
  * Creates the Google Analytics service
  */
 $app['google_api_service'] = $app->share(function ($app) {
     try {
+        $c = $app['config']->google_api;
         $client = new Google_Client();
-        $client->setApplicationName($app['google_api_app_name']);
+        $client->setApplicationName($c->app_name);
         $service = new Google_Service_Analytics($client);
 
         if (null !== $gapi = $app['session']->get('gapi')) {
             $client->setAccessToken($gapi['token']);
         }
 
-        $key = file_get_contents($app['google_api_key_path']);
+        $key = file_get_contents(__DIR__.$c->key_path);
         $cred = new Google_Auth_AssertionCredentials(
-            $app['google_api_username'],
+            $c->username,
             array(Google_Service_Analytics::ANALYTICS_READONLY),
             $key,
-            $app['google_api_key_secret']
+            $c->key_secret
         );
 
         $client->setAssertionCredentials($cred);
@@ -61,7 +73,7 @@ $app['google_api_service'] = $app->share(function ($app) {
         $app['session']->set('gapi', array('token' => $client->getAccessToken()));
     } catch (Google_Exception $e) {
         $app['monolog']->addError($e->getMessage());
-        die();
+        exit(1);
     }
 
     return $service;
@@ -72,23 +84,21 @@ $app['google_api_service'] = $app->share(function ($app) {
  */
 $app['websites'] = $app->share(function ($app) {
     /* @var Google_Service_Analytics $service */
+    $c = $app['config'];
     $service = $app['google_api_service'];
     $websites = array();
 
-    $json = file_get_contents($app['websites_path']);
-    $tracking_codes = json_decode($json, true);
-    $tracking_codes = $tracking_codes['websites'];
-
+    $tracking_codes = $c->websites;
     try {
         $summaries = $service->management_accountSummaries->listManagementAccountSummaries();
         foreach ($summaries->getItems() as $account) {
             $web_properties = $service->management_webproperties->listManagementWebproperties($account->getId());
             foreach ($tracking_codes as $code) {
                 foreach ($web_properties as $property) {
-                    if ($property->getId() !== $code['tracking_id']) {
+                    if ($property->getId() !== $code->tracking_id) {
                         continue;
                     }
-                    $websites[$code['name']] = 'ga:'.$property->getDefaultProfileId();
+                    $websites[$code->name] = 'ga:'.$property->getDefaultProfileId();
                     break;
                 }
             }
@@ -104,7 +114,7 @@ $app['websites'] = $app->share(function ($app) {
  * Returns a CSV with the vistors for each website in the last 24 hours
  * Format: website;dateHour;users
  */
-$app->get('/api/getuserslastday', function (Silex\Application $app) {
+$app->get('/api/getuserslastday.csv', function (Silex\Application $app) {
     /* @var Google_Service_Analytics $service */
     $service = $app['google_api_service'];
     $websites = $app['websites'];
@@ -113,22 +123,38 @@ $app->get('/api/getuserslastday', function (Silex\Application $app) {
     $params = array(
         'dimensions' => 'ga:dateHour',
         'sort' => '-ga:dateHour',
-        'max-results' => '24',
+        'max-results' => '25',
     );
     foreach ($websites as $name => $code) {
-        $vistors = null;
+        $visitors = null;
         try {
-            $vistors = $service->data_ga->get($code, 'yesterday', 'today', 'ga:users', $params);
+            $visitors = $service->data_ga->get($code, 'yesterday', 'today', 'ga:users', $params);
+
+            $results = array();
+            foreach ($visitors->getRows() as $row) {
+                if (count($row) !== 2) {
+                    continue;
+                }
+                $results[$row[0]] = $row[1];
+            }
+
+            // Backfill the data
+            $current_datetime = new \DateTime('24 hours ago');
+            $end_date = new \DateTime('now');
+
+            while ($current_datetime < $end_date) {
+                $formated_date = $current_datetime->format('YmdH');
+                $users = (array_key_exists($formated_date, $results)) ? $results[$formated_date] : 0;
+                $data[] = array(
+                    'website' => $name,
+                    'dateHour' => $formated_date,
+                    'users' => $users,
+                );
+                $current_datetime->add(new DateInterval('PT1H'));
+            }
         } catch (Google_Exception $e) {
             $app['monolog']->addError($e->getMessage());
-        }
-
-        foreach ($vistors->getRows() as $row) {
-            $data[] = array(
-                'website' => $name,
-                'dateHour' => $row[0],
-                'users' => $row[1],
-            );
+            continue;
         }
     }
 
@@ -146,40 +172,38 @@ $app->get('/api/getuserslastday', function (Silex\Application $app) {
  * Returns a CSV with the active vistors for each website
  * Format: website;users
  */
-$app->get('/api/getactiveusers', function (Silex\Application $app) {
+$app->get('/api/getactiveusers.json', function (Silex\Application $app) {
     /* @var Google_Service_Analytics $service */
     $service = $app['google_api_service'];
     $websites = $app['websites'];
     $data = array();
 
     foreach ($websites as $name => $code) {
-        $vistors = null;
         try {
-            $vistors = $service->data_realtime->get($code, 'rt:activeUsers');
+            $visitors = $service->data_realtime->get($code, 'rt:activeUsers');
+            $data[$name] = ($visitors->getTotalResults() > 0) ? $visitors->getRows()[0][0] : 0;
         } catch (Google_Exception $e) {
             $app['monolog']->addError($e->getMessage());
         }
-
-        foreach ($vistors->getRows() as $row) {
-            $data[] = array(
-                'website' => $name,
-                'users' => $row[0],
-            );
-        }
     }
 
-    $content = $app['twig']->render('api/getactiveusers.csv.twig', array(
+    $content = $app['twig']->render('api/getactiveusers.json.twig', array(
         'data' => $data,
     ));
 
-    $headers = array('Content-Type' => 'text/csv');
+    $headers = array('Content-Type' => 'application/json');
     $response = new Response($content, 200, $headers);
 
     return $response;
 });
 
 $app->get('/', function (Silex\Application $app) {
-    return $app['twig']->render('default/index.html.twig', array());
+    $c = $app['config']->display;
+
+    return $app['twig']->render('default/index.html.twig', array(
+        'horizontal_tiles' => $c->horizontal_tiles,
+        'vertical_tiles' => $c->vertical_tiles,
+    ));
 });
 
 $app->run();
